7d7a45791eb664d5888b914ba1c4a5220bde8cdf
Samuel Gichu <itsgrapes@mb96.co>
2020-06-01 14:31:13 +0300

    ...

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Login | Seven Hills</title>
+    <title></title>
 
     <!-- Scripts -->
     <script src="{{ asset('public/js/app.js') }}" defer></script>
