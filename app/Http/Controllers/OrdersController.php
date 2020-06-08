6090e7f3eee7526ceac49b3afcba52938abc2d1c
Samuel Gichu <itsgrapes@mb96.co>
2020-06-08 21:55:48 +0300

    Added Order management endpoint

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class OrdersController extends Controller
+{
+    //Order Management
+    public function store(Request $request){
+      $request
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,6 @@ Route::get('products/{product}', 'ProductsController@show');
 Route::post('products', 'ProductsController@store');
 Route::put('products/{product}', 'ProductsController@update');
 Route::delete('products/{product}', 'ProductsController@delete');
+
+//orders endpoints
+Route::post('orders', 'OrdersController@store');
